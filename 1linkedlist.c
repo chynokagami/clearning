499ded76eca99702b7c5d3ee1acdeba2c45ef66c
Martin_Chen <chynokagami@gmail.com>
2025-10-27 05:40:09 +0900

    5.40

--- 1linkedlist.c
+++ 1linkedlist.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
+#include <stdlib.h>
+
+typedef struct LNode{
+  int data;
+  struct LNode *next;
+}LNode;
+
+LNode *initList(){
+  // 创建头结点，L为头指针
+  LNode *L = (LNode *)malloc(sizeof(LNode));
+  L->next = NULL;  // 指针域置空
+  return L;        // 返回头指针
+}
 int main(){
-  typedef struct LNode{
-    int data;
-    struct LNode *next;
-  }LNode;
-  printf("i got github");
+
   return 0;
 }
