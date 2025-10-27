4cc86e2198974fef917415152e9e6bb8937a75a2
Martin_Chen <chynokagami@gmail.com>
2025-10-28 04:50:17 +0900

    4.38

---
+++ cmake-build-debug/CMakeFiles/1linkedlist.c.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "1linkedlist.c.exe"
+  "1linkedlist.c.exe.manifest"
+  "1linkedlist.c.pdb"
+  "CMakeFiles/1linkedlist.c.dir/1linkedlist.c.obj"
+  "CMakeFiles/1linkedlist.c.dir/1linkedlist.c.obj.d"
+  "lib1linkedlist.c.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/1linkedlist.c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/test.c.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.c.dir/test.c.obj"
+  "CMakeFiles/test.c.dir/test.c.obj.d"
+  "libtest.c.dll.a"
+  "test.c.exe"
+  "test.c.exe.manifest"
+  "test.c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test.c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
